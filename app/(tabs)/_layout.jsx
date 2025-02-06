84d84654864cd9ddd8b964c224b19f8ec5b64b21
Rineta2 <rzkir571@gmail.com>
2025-02-06 08:18:18 +0700

    [update] Firt Setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import { Tabs } from "expo-router";
+
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="AddNew"
+        options={{
+          tabBarLabel: "Add New",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
